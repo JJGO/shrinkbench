ddef3a8d744c703fa4d2fa6f86ba07fec7688b7e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-08-22 10:39:54 -0400

    Add mnist dataset

---
+++ datasets/mnist.py
@@ -0,0 +1,37 @@
+from torchvision import transforms, datasets
+
+from . import get_data_paths
+
+normalize = transforms.Normalize((0.1307,), (0.3081,))
+
+
+def train_dataset(preproc=True):
+
+    data_paths = get_data_paths()
+
+    train_preprocessing = None
+    if preproc:
+        train_preprocessing = transforms.Compose([transforms.ToTensor(), normalize])
+
+    train_dataset = datasets.MNIST(data_paths['MNIST'],
+                                   'train',
+                                   transforms=train_preprocessing)
+    return train_dataset
+
+
+def val_dataset(preproc=True):
+
+    data_paths = get_data_paths()
+
+    val_preprocessing = None
+    if preproc:
+        val_preprocessing = transforms.Compose([transforms.ToTensor(), normalize])
+
+    val_dataset = datasets.MNIST(data_paths['MNIST'],
+                                 'val',
+                                 transforms=val_preprocessing)
+    return val_dataset
+
+
+def get_datasets(preproc=True):
+    return train_dataset(preproc), val_dataset(preproc)
