29cd474b43c8a05b699db2a4d1938dbe3be00d4e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-08-28 02:15:37 -0400

    Include CUDA deterministic options

--- experiment/seed.py
+++ experiment/seed.py
@@ -14,3 +14,9 @@ def fix_seed(seed=42):
 
     # PyTorch
     torch.manual_seed(seed)
+
+
+    # TODO Enable full deterministic computation
+    # https://pytorch.org/docs/stable/notes/randomness.html
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
