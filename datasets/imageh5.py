48d3f644ff81277f580e64f7e257c0d779aae157
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-08-31 11:11:03 -0400

    Add support for HDF5 PIL datasets with hierarchy

--- datasets/imageh5.py
+++ datasets/imageh5.py
@@ -1,21 +1,30 @@
+import pathlib
+
 import h5py
 import PIL.Image
+import numpy as np
 from torch.utils.data import Dataset
+from tqdm import tqdm
 
 
 def make_dataset(hf):
 
     samples = []
-    class_to_idx = {}
-
-    for label, class_ in enumerate(sorted(hf)):
-
-        class_to_idx[class_] = label
+    for label, class_ in enumerate(tqdm(sorted(hf), leave=False)):
 
         for image in sorted(hf[class_]):
-            samples.append( (f"{class_}/{image}", label) )
+            samples.append( (hf[class_][image].name, label) )
 
-    return samples, class_to_idx
+    return samples
+
+
+def class_to_idx(hf):
+
+    class_to_idx = {}
+    for label, class_ in enumerate(sorted(hf)):
+        class_to_idx[class_] = label
+
+    return class_to_idx
 
 
 def PIL_loader(array):
@@ -28,19 +37,32 @@ class ImageHDF5Dataset(Dataset):
                  transform=None,  target_transform=None):
         super(ImageHDF5Dataset, self).__init__()
 
-        self.hf = h5py.File(h5file, 'r')
+        self.h5file = pathlib.Path(h5file)
+        indexfile = h5file.with_suffix('.idx.npy')
+
         self.loader = loader
         self.transform = transform
         self.target_transform = target_transform
         # Compile image & label list
-        self.samples, self.class_to_idx = make_dataset(self.hf)
+        if indexfile.exists():
+            self.samples = [(x, int(y)) for x, y in np.load(indexfile)]
+        else:
+            print("Compiling dataset indices")
+            with h5py.File(self.h5file, 'r') as hf:
+                self.samples = make_dataset(hf)
+                np.save(indexfile, self.samples)
+            # self.class_to_idx = class_to_idx(hf)
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, index):
+        # File descriptor must be opened here because h5 does
+        # support processing inheriting the file descriptor
+        # https://gist.github.com/bkj/f448025fdef08c0609029489fa26ea2a
         path, target = self.samples[index]
-        sample = self.loader(self.hf[path].value)
+        with h5py.File(self.h5file, 'r') as hf:
+            sample = self.loader(hf[path].value)
         if self.transform is not None:
             sample = self.transform(sample)
         if self.target_transform is not None:
@@ -48,5 +70,42 @@ class ImageHDF5Dataset(Dataset):
 
         return sample, target
 
-    def __del__(self):
-        self.hf.close()
+
+# class ImageHDF5Dataset(Dataset):
+
+#     def __init__(self, h5file, loader=PIL_loader,
+#                  transform=None,  target_transform=None):
+#         super(ImageHDF5Dataset, self).__init__()
+
+#         h5file = pathlib.Path(h5file)
+#         indexfile = h5file.with_suffix('.idx.npy')
+
+#         self.hf = h5py.File(h5file, 'r')
+#         self.loader = loader
+#         self.transform = transform
+#         self.target_transform = target_transform
+#         # Compile image & label list
+#         if indexfile.exists():
+#             self.samples = [(x, int(y)) for x, y in np.load(indexfile)]
+#         else:
+#             print("Compiling dataset indices")
+#             self.samples = make_dataset(self.hf)
+#             np.save(indexfile, self.samples)
+#         self.class_to_idx = class_to_idx(self.hf)
+
+#     def __len__(self):
+#         return len(self.samples)
+
+#     def __getitem__(self, index):
+#         path, target = self.samples[index]
+#         sample = self.loader(self.hf[path].value)
+#         if self.transform is not None:
+#             sample = self.transform(sample)
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+
+#         return sample, target
+
+#     def __del__(self):
+#         if hasattr(self, 'hf'):
+#             self.hf.close()
