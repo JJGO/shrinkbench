e6e69787e44e3c62951343ff8eb1b9345759e871
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-09-02 20:49:45 -0400

    Refactor masks to support new api

--- pruning/mask.py
+++ pruning/mask.py
@@ -1,32 +1,8 @@
+import numpy as np
+import pandas as pd
+
 from .modules import masked_modules
-
-
-def get_modules(module, prefix=""):
-    """Recursively find all submodules from torch modules,
-    returning them in state_dict format
-    """
-    modules = {}
-    for n, m in module.named_children():
-        modules[prefix+n] = m
-        modules.update(get_modules(m, prefix=prefix+n+'.'))
-    return modules
-
-
-def compute_masks(model, strategy):
-    """Compute masks using the said strategy for every model
-    This is a straight forward implementation that supports
-    strategies that prune each module independently
-    """
-    masks = {}
-
-    modules = get_modules(model)
-
-    for name, module in modules.items():
-        masks_ = strategy.module_masks(module)
-        if len(masks_) > 0:
-            masks[name] = masks_
-
-    return masks
+from ..models.head import get_classifier_module
 
 
 def mask_module(module, masks, prefix=""):
@@ -50,3 +26,40 @@ def mask_module(module, masks, prefix=""):
         setattr(module, name, masked)
 
     return module
+
+
+def unmask_classifier(model, masks):
+
+    clf = get_classifier_module(model)
+
+    if clf in masks:
+        for p in masks[clf]:
+            masks[clf][p] = np.ones_like(masks[clf][p])
+
+    return masks
+
+
+def masks_details(masks):
+    rows = []
+    for module, module_masks in masks.items():
+        for k, v in module_masks.items():
+            rows.append([module, k, 1/v.mean(), np.prod(v.shape), v.shape])
+    columns = ['module', 'param', 'comp', 'size', 'shape']
+    return pd.DataFrame(rows, columns=columns)
+
+# from .utils import get_modules
+# def compute_masks(model, strategy):
+#     """Compute masks using the said strategy for every model
+#     This is a straight forward implementation that supports
+#     strategies that prune each module independently
+#     """
+#     masks = {}
+
+#     modules = get_modules(model)
+
+#     for name, module in modules.items():
+#         masks_ = strategy.module_masks(module)
+#         if len(masks_) > 0:
+#             masks[name] = masks_
+
+#     return masks
