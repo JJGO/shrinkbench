d256f4dbde8be393c85ce0198c23ffb0eccb7104
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-02-28 13:54:06 -0500

    Document flops and add sizeof support

--- metrics/flops.py
+++ metrics/flops.py
@@ -1,15 +1,19 @@
 import numpy as np
 from torch import nn
 
-from . import nonzero, dtype2bits
-from .raw_flops import dense_flops, conv2d_flops
-from ..pruning.utils import get_activations, get_params
+from . import nonzero
+from .abstract_flops import dense_flops, conv2d_flops
+from ..pruning.utils import get_activations
 from ..pruning import Conv2dMasked, LinearMasked
 
 
 def _conv2d_flops(module, activation):
-    input_shape = activation.shape[2:]  # Ignore batch & channels
-    # TODO fix dilation and padding size
+    # Auxiliary func to use abstract flop computation
+
+    # Drop batch & channels. Channels can be dropped since
+    # unlike shape they have to match to in_channels
+    input_shape = activation.shape[2:]
+    # TODO Add support for dilation and padding size
     return conv2d_flops(in_channels=module.in_channels,
                         out_channels=module.out_channels,
                         input_shape=input_shape,
@@ -20,28 +24,40 @@ def _conv2d_flops(module, activation):
 
 
 def _linear_flops(module, activation):
+    # Auxiliary func to use abstract flop computation
     return dense_flops(module.in_features, module.out_features)
 
 
 def flops(model, input):
+    """Compute Multiply-add FLOPs estimate from model
 
+    Arguments:
+        model {torch.nn.Module} -- Module to compute flops for
+        input {torch.Tensor} -- Input tensor needed for activations
+
+    Returns:
+        tuple:
+        - int - Number of total FLOPs
+        - int - Number of FLOPs related to nonzero parameters
+    """
     FLOP_fn = {
         nn.Conv2d: _conv2d_flops,
-        Conv2dMasked: _conv2d_flops,
         nn.Linear: _linear_flops,
+        Conv2dMasked: _conv2d_flops,
         LinearMasked: _linear_flops,
     }
 
-    # Multiply-add flops
     total_flops = nonzero_flops = 0
     activations = get_activations(model, input)
 
     # The ones we need for backprop
     for m, (act, _) in activations.items():
         if m.__class__ in FLOP_fn:
-            w = m.weight.detach().cpu().numpy()
+            w = m.weight.detach().cpu().numpy().copy()
             module_flops = FLOP_fn[m.__class__](m, act)
             total_flops += module_flops
+            # For our operations, all weights are symmetric so we can just
+            # do simple rule of three for the estimation
             nonzero_flops += module_flops * nonzero(w).sum() / np.prod(w.shape)
 
     return total_flops, nonzero_flops
