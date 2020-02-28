c97c50fa7f3b22c75cd4d01c9ba0208fa03f2748
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-02-28 13:55:48 -0500

    Document CSV logger

--- util/csvlogger.py
+++ util/csvlogger.py
@@ -1,3 +1,5 @@
+"""Custom CSV Logger
+"""
 import csv
 import torch
 
@@ -5,6 +7,17 @@ import torch
 class CSVLogger:
 
     def __init__(self, file, columns):
+        """General purpose CSV Logger
+
+        Initialized with a set of columns, it then has two operations
+          - set(**kwargs) - to add entries into the current row
+          - update - flush a row to file
+
+        Arguments:
+            file {str} -- Path to file
+            columns {List[str]} -- List of keys that CSV is going to log
+        """
+
         self.file = open(file, 'w')
         self.columns = columns
         self.values = {}
@@ -14,6 +27,16 @@ class CSVLogger:
         self.file.flush()
 
     def set(self, **kwargs):
+        """Set value for current row
+
+        [description]
+
+        Arguments:
+            **kwargs {[type]} -- [description]
+
+        Raises:
+            ValueError -- [description]
+        """
         for k, v in kwargs.items():
             if k in self.columns:
                 if isinstance(v, torch.Tensor):
@@ -23,9 +46,13 @@ class CSVLogger:
                 raise ValueError(f"{k} not in columns {self.columns}")
 
     def update(self):
+        """Take current values and write a row in the CSV
+        """
         row = [self.values.get(c, "") for c in self.columns]
         self.writer.writerow(row)
         self.file.flush()
 
     def close(self):
+        """Close the file descriptor for the CSV
+        """
         self.file.close()
