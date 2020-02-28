aa976af1583914ca0dc316822f949e9e7bc06201
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-02-28 14:01:45 -0500

    Limit scope of head.py to torchvision.
    Refactor using .is_classifier attribute

--- models/head.py
+++ models/head.py
@@ -1,11 +1,47 @@
+"""Auxiliary module for dealing with classifier layers in networks
+"""
 from torch import nn
 import torchvision.models
 
+# classifier is .fc
+MODELS_WITH_FC = (
+    torchvision.models.ResNet,
+    torchvision.models.GoogLeNet,
+    torchvision.models.ShuffleNetV2,
+)
 
-# TODO refactor replace_head and get_classifier_params into one func
+# classifier is .classifier
+MODELS_WITH_CLASSIFIER = (
+    torchvision.models.DenseNet,
+)
+
+
+# classifier is .classifier[-1]
+MODELS_WITH_CLASSIFIER_LIST = (
+    torchvision.models.AlexNet,
+    torchvision.models.VGG,
+    torchvision.models.MobileNetV2,
+    torchvision.models.MNASNet,
+)
 
 
 def reduce_linear_layer(layer, n_classes, keep_weights=False):
+    """Gets a torch.nn.Linear layer and resizes it to a new number of classes
+
+    The keep_weights argument is useful for experiments that subset an
+    existing dataset. E.g. First 10 or 100 classes of ImageNet.
+    This is helpful when debugging pipelines
+
+    Arguments:
+        layer {nn.Linear} -- Dense Classifier Layer
+        n_classes {int} -- New Number of layers
+
+    Keyword Arguments:
+        keep_weights {bool} -- Whether to keep previous weights (default: {False})
+
+    Returns:
+        [nn.Linear] -- New Dense layer for classification
+    """
     assert isinstance(layer, nn.Linear)
     new_layer = nn.Linear(layer.in_features, n_classes)
     if keep_weights:
@@ -15,42 +51,27 @@ def reduce_linear_layer(layer, n_classes, keep_weights=False):
 
 
 def replace_head(model, n_classes, keep_weights=True):
+    """Replace the classifier layer with a different one
 
-    from .. import models as custom_models
+    This is needed when repurposing an architecture for a different task
+    For example using an ImageNet designed ResNet 50 for Places365
 
-    # classifier is .fc
-    MODELS_WITH_FC = (
-        torchvision.models.ResNet,
-        torchvision.models.GoogLeNet,
-        torchvision.models.ShuffleNetV2,
-    )
+    Arguments:
+        model {nn.Module} -- Classification Network
+        n_classes {int} -- New number of classes
 
-    # classifier is .classifier
-    MODELS_WITH_CLASSIFIER = (
-        torchvision.models.DenseNet,
-    )
+    Keyword Arguments:
+        keep_weights {bool} -- Whether to keep previous weights. See reduce_linear_layer (default: {True})
 
-    # classifier is .linear
-    MODELS_WITH_LINEAR = (
-        custom_models.cifar_resnet.ResNet
-    )
-
-    # classifier is .classifier[-1]
-    MODELS_WITH_CLASSIFIER_LIST = (
-        torchvision.models.AlexNet,
-        torchvision.models.VGG,
-        torchvision.models.MobileNetV2,
-        torchvision.models.MNASNet,
-    )
+    Raises:
+        NotImplementedError -- Raised when the architecture class is not supported
+    """
 
     # keep_weights is for imagenet subdatasets where
     # one samples the first N
 
     if isinstance(model, MODELS_WITH_FC):
         model.fc = reduce_linear_layer(model.fc, n_classes, keep_weights)
-
-    elif isinstance(model, MODELS_WITH_LINEAR):
-        model.linear = reduce_linear_layer(model.linear, n_classes, keep_weights)
 
     elif isinstance(model, MODELS_WITH_CLASSIFIER):
         model.classifier = reduce_linear_layer(model.classifier, n_classes, keep_weights)
@@ -60,53 +81,35 @@ def replace_head(model, n_classes, keep_weights=True):
                                                    n_classes, keep_weights)
 
     elif isinstance(model, torchvision.models.SqueezeNet):
-        # Weird, uses convs
+        # TODO: Non standard, uses convs
         raise NotImplementedError()
 
     elif isinstance(model, torchvision.models.Inception3):
-        # Weird, expects 299 and aux outputs
+        # TODO: Non standard, expects 299 and aux outputs
         raise NotImplementedError()
 
     # TODO include efficientnet
 
     else:
-        raise ValueError(f"Model {model} not recognized")
+        raise NotImplementedError(f"Model {model} not supported")
 
 
 def get_classifier_module(model):
-    from .. import models as custom_models
+    """Get classifier module for many Vision Classification networks
 
-    # classifier is .fc
-    MODELS_WITH_FC = (
-        torchvision.models.ResNet,
-        torchvision.models.GoogLeNet,
-        torchvision.models.ShuffleNetV2,
-    )
 
-    # classifier is .classifier
-    MODELS_WITH_CLASSIFIER = (
-        torchvision.models.DenseNet,
-    )
+    Arguments:
+        model {nn.Module} -- Network to extract the classifier from
 
-    # classifier is .linear
-    MODELS_WITH_LINEAR = (
-        custom_models.cifar_resnet.ResNet
-    )
+    Returns:
+        [nn.Linear] -- Dense Layer that performs the last step of classification
 
-    # classifier is .classifier[-1]
-    MODELS_WITH_CLASSIFIER_LIST = (
-        torchvision.models.AlexNet,
-        torchvision.models.VGG,
-        torchvision.models.MobileNetV2,
-        torchvision.models.MNASNet,
-        custom_models.cifar_vgg.VGGBnDrop,
-    )
+    Raises:
+        NotImplementedError -- Raised when the architecture class is not supported
+    """
 
     if isinstance(model, MODELS_WITH_FC):
         clf = 'fc'
-
-    elif isinstance(model, MODELS_WITH_LINEAR):
-        clf = 'linear'
 
     elif isinstance(model, MODELS_WITH_CLASSIFIER):
         clf = 'classifier'
@@ -116,19 +119,23 @@ def get_classifier_module(model):
         clf = f"classifier.{i}"
 
     elif isinstance(model, torchvision.models.SqueezeNet):
-        # Weird, uses convs
+        # TODO: Non standard, uses convs
         raise NotImplementedError()
 
     elif isinstance(model, torchvision.models.Inception3):
-        # Weird, expects 299 and aux outputs
+        # TODO: Non standard, expects 299 and aux outputs
         raise NotImplementedError()
 
     # TODO include efficientnet
 
-    elif isinstance(model, custom_models.mnistnet.MnistNet):
-        clf = 'fc2'
-
     else:
-        raise ValueError(f"Model {model} not recognized")
+        raise NotImplementedError(f"Model {model} not recognized")
 
     return clf
+
+
+def mark_classifier(model):
+    # Mark it manually for torchvision models
+    clf_module = get_classifier_module(model)
+    clf_module.is_classifier = True
+    return model
