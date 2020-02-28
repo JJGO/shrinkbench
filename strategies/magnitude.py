be189104e6509684abd32c059e8a92c321386ae7
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-02-28 14:10:44 -0500

    Major refactor of baseline strategies

--- strategies/magnitude.py
+++ strategies/magnitude.py
@@ -1,69 +1,95 @@
-# -*- coding: utf-8 -*-
-
 """Magnitude based pruning
 
 Implements pruning strategy that prunes as follows
 
 For a given tensor, sort by absolute value and
-keep only the _fraction_ with highest magnitudes.
+keep only the _fraction_ with highest magnitudes
+so that overall desired compression is achieved
 """
 
 import numpy as np
-import torch.nn as nn
 
-from ..pruning import *
-from .utils import abs_threshold, largest_abs_mask
+from ..pruning import (LayerPruning,
+                       VisionPruning,
+                       GradientMixin,
+                       ActivationMixin)
+from .utils import (fraction_threshold,
+                    fraction_mask,
+                    map_importances,
+                    flatten_importances,
+                    importance_masks,
+                    activation_importance)
 
 
-class GlobalMagnitudePruning(Pruning):
+class GlobalMagWeight(VisionPruning):
 
-    def __init__(self, compression, prune_classifier=False):
-        super(GlobalMagnitudePruning, self).__init__(compression=compression,
-                                                     prune_classifier=prune_classifier)
-        self.masked_modules = (nn.Linear, nn.Conv2d)
-
-    def model_masks(self, model, *_):
-
-        prunable = prunable_modules(model,
-                                    self.masked_modules,
-                                    self.prune_classifier)
-        fraction = fraction_to_keep(self.compression, model, prunable)
-
-        flat_params = np.concatenate([
-            param.flatten() for module in prunable
-            for _, param in get_params(module).items()
-            ])
-
-        threshold = abs_threshold(flat_params, fraction)
-
-        masks = { module :
-                  { k : largest_abs_mask(v, threshold=threshold)
-                    for k, v in get_params(module).items() }
-                  for module in prunable}
+    def model_masks(self):
+        importances = map_importances(np.abs, self.params())
+        flat_importances = flatten_importances(importances)
+        threshold = fraction_threshold(flat_importances, self.fraction)
+        masks = importance_masks(importances, threshold)
         return masks
 
 
-class LayerMagnitudePruning(LayerPruning):
+class LayerMagWeight(LayerPruning, VisionPruning):
 
-    def __init__(self, compression, prune_classifier=False):
-        super(LayerMagnitudePruning, self).__init__(compression=compression,
-                                                    prune_classifier=prune_classifier)
-        self.masked_modules = (nn.Linear, nn.Conv2d)
-
-    def module_masks(self, module):
-        masks = {}
-        if isinstance(module, self.masked_modules):
-            params = get_params(module)
-            masks['weight'] = largest_abs_mask(params['weight'], fraction=self.fraction)
-            if module.bias is not None:
-                masks['bias'] = largest_abs_mask(params['bias'], fraction=self.fraction)
+    def layer_masks(self, module):
+        params = self.module_params(module)
+        importances = {param: np.abs(value) for param, value in params.items()}
+        masks = {fraction_mask(importances[param], self.fraction)
+                 for param, value in params.items() if value is not None}
         return masks
 
-    def model_masks(self, model, *_):
-        prunable = prunable_modules(model,
-                                    self.masked_modules,
-                                    self.prune_classifier)
-        self.fraction = fraction_to_keep(self.compression, model, prunable)
-        masks = super(LayerMagnitudePruning, self).model_masks(model, prunable=prunable)
-        delattr(self, 'fraction')
+
+class GlobalMagGrad(GradientMixin, VisionPruning):
+
+    def model_masks(self):
+        params = self.params()
+        grads = self.param_gradients()
+        importances = {mod:
+                       {p: np.abs(params[mod][p]*grads[mod][p])
+                        for p in mod_params}
+                       for mod, mod_params in params.items()}
+        flat_importances = flatten_importances(importances)
+        threshold = fraction_threshold(flat_importances, self.fraction)
+        masks = importance_masks(importances, threshold)
+        return masks
+
+
+class LayerMagGrad(GradientMixin, LayerPruning, VisionPruning):
+
+    def layer_masks(self, module):
+        params = self.module_params(module)
+        grads = self.module_param_gradients(module)
+        importances = {param: np.abs(value*grads[param]) for param, value in params.items()}
+        masks = {fraction_mask(importances[param], self.fraction)
+                 for param, value in params.items() if value is not None}
+        return masks
+
+
+class GlobalMagAct(ActivationMixin, VisionPruning):
+
+    def model_masks(self):
+        params = self.params()
+        activations = self.activations()
+        # [0] is input activation
+        importances = {mod:
+                       {p: np.abs(activation_importance(params[mod][p], activations[mod][0]))
+                        for p in mod_params}
+                       for mod, mod_params in params.items()}
+        flat_importances = flatten_importances(importances)
+        threshold = fraction_threshold(flat_importances, self.fraction)
+        masks = importance_masks(importances, threshold)
+        return masks
+
+
+class LayerMagAct(ActivationMixin, LayerPruning, VisionPruning):
+
+    def layer_masks(self, module):
+        params = self.module_params(module)
+        input_act, _ = self.module_activations(module)
+        importances = {param: np.abs(activation_importance(value, input_act))
+                       for param, value in params.items()}
+        masks = {fraction_mask(importances[param], self.fraction)
+                 for param, value in params.items() if value is not None}
         return masks
