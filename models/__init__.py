a700a65472d6c872585f15b8afe41ee92aad16d2
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-06-09 15:03:45 -0400

    Closes #19

--- models/__init__.py
+++ models/__init__.py
@@ -10,11 +10,5 @@ from .cifar_resnet import (resnet20,
                            resnet56,
                            resnet110,
                            resnet1202)
-from .cifar_resnet import (resnet20_100,
-                           resnet32_100,
-                           resnet44_100,
-                           resnet56_100,
-                           resnet110_100,
-                           resnet1202_100)
 
 from .cifar_vgg import vgg_bn_drop, vgg_bn_drop_100
